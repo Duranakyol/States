ef69c4f70983f4ff658877f5ff52de66d1d4b55d
Duranakyol <duranakyol71@gmail.com>
2022-07-04 10:10:15 +0200

    states

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState }  from 'react';
 import './App.css';
+import Counter from './components/Counter'
 
 function App() {
+
+  const [name, setName] = useState("Duran");
+  const [age, setAge] = useState(27);
+  const [friends, setFriends] = useState(["Ahmet", "Murat"]);
+  const [adress, setAdress] = useState({ title: "Berlin" , PLZ: 12353 })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> {name} </div>
+      <div> { age } </div>
+      <button onClick={() => setName("Kazim")}>NameChange</button><br/>
+      <button onClick={() => setAge(31)}>AgeChange</button>
+
+      <hr />
+      <h2>Friends :</h2>
+      {
+        friends.map((friend, index) => {
+          return (
+            <div> 
+              <div key={index}>{friend}</div>
+            </div>  
+          )
+        })
+      }
+      <button onClick={() => setFriends([...friends, 'Ayse'])}>Add Friend</button><br />
+      <h2>ADRESS :</h2>
+      <div> {adress.title} {adress.PLZ} </div>
+      <button onClick={() => setAdress({ title: "Ankara", PLZ: 71100 })}>AdressChange</button><br />
+      <hr/>
+      <Counter/>
     </div>
   );
 }
